2bd50762e4b68d06ec233834289848aaf197ba1c
sashaij <o.zagorodniuk@outlook.com>
2024-02-16 13:41:45 +0200

    Style configuration in progress

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,21 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: path.resolve(__dirname, 'src/index.js'),
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js'),
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+        filename: '[name].js',
     },
-
+    module: {
+        rules: [
+            {
+                test:/\.css$/,
+                use: [
+                    'style-loader', 'css-loader', 'sass-loader',
+                ],
+            }
+        ]
+    }
 }
