09f0189d48c1793d34fcfccd254ce17255961da1
sashaij <o.zagorodniuk@outlook.com>
2024-02-17 11:10:12 +0200

    Add html loader and template html file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -18,5 +19,12 @@ module.exports = {
                 ],
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack App',
+            filename: 'index.html',
+            template: 'src/template.html',
+        })
+    ]
 }
