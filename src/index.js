71f12f1a806a9be8ad24b2665ceeb50c24da54b0
sashaij <o.zagorodniuk@outlook.com>
2024-02-18 14:28:44 +0200

    Add app structure scheme

--- src/index.js
+++ src/index.js
@@ -1,1 +1,91 @@
 import './styles/main.css' 
+
+//lists
+    //lists section
+        //list
+            //list items
+    //add item 
+        //input field
+        //'add' button 
+
+//container
+    //title
+        //flex items
+        //flex direction column
+
+//add list function 
+    //call function
+    //dynamically add list item
+        //container (button?)
+          //name
+
+//list
+    //container
+        //title section
+            //title
+            //number of remaining tasks
+        //list section
+            //list item
+                //checkbox
+                //title
+                //delete button
+                //modify button
+        //add item section
+            //input field
+            //'add item' button   
+        //option buttons section 
+            //show/hide completed tasks
+            //delete list 
+
+
+//functionality
+
+//lists
+
+    //add list to list item
+        //call function 
+        //add item to a list
+        //console log list
+
+
+
+    //delete item from list
+        //delete item 
+        //console.log remaining items
+
+    //delete list
+        //call function
+        //remove closest div/list item
+    
+    
+      
+//tasks
+
+      //show items in the list
+        //call function 
+        //console.log remaining items in the list
+
+
+    //show remaining tasks
+        //call function 
+        //count items in the list
+        //console a message with number of tasks
+
+    //delete task
+        //delete closest div/list item
+
+    //modify task
+        //click on target
+            //display text field with
+            //original text
+            //modify text
+            //element > innerhtml >
+            //> 'new text'
+            
+
+
+        
+    
+
+    
+
